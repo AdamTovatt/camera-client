c39cf2d04e182d4f9408c18abeea6086cd314311
Adam Tovatt <sporedenstore@hotmail.com>
2023-05-20 21:43:34 +0200

    add debug messages

--- ServoController.py
+++ ServoController.py
@@ -18,10 +18,12 @@ def setup():
     global factory, pitchServo, yawServo  # Declare the variables as global
 
     factory = PiGPIOFactory()
-    pitchServo = Servo(14, min_pulse_width=0.5/1000,
-                       max_pulse_width=2.5/1000, pin_factory=factory)
-    yawServo = Servo(15, min_pulse_width=0.5/1000,
-                     max_pulse_width=2.5/1000, pin_factory=factory)
+    # pitchServo = Servo(14, min_pulse_width=0.5/1000,
+    #                   max_pulse_width=2.5/1000, pin_factory=factory)
+    # yawServo = Servo(15, min_pulse_width=0.5/1000,
+    #                 max_pulse_width=2.5/1000, pin_factory=factory)
+    pitchServo = Servo(14, pin_factory=factory)
+    yawServo = Servo(15, pin_factory=factory)
 
 
 def movePosition(deltaPitch, deltaYaw):
@@ -35,6 +37,9 @@ def movePosition(deltaPitch, deltaYaw):
 
     pitchValue = limit(pitchValue + pitchAngle)
     yawValue = limit(yawValue + yawAngle)
+
+    print("Setting pitch to " + str(pitchValue) +
+          " and yaw to " + str(yawValue))
 
     pitchServo.value = pitchValue
     yawServo.value = yawValue
