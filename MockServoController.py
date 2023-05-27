045617adb7c584baf4ce96e39774e29d94377dde
Adam Tovatt <sporedenstore@hotmail.com>
2023-05-27 17:38:43 +0200

    handle move on separate thread

---
+++ MockServoController.py
@@ -0,0 +1,2 @@
+def movePosition(newPitch, newYaw):
+    print("Pitch: " + str(newPitch) + ", Yaw: " + str(newYaw))
--- StreamVideo.py
+++ StreamVideo.py
@@ -1,3 +1,4 @@
+import threading
 import cv2
 import websocket
 import struct
@@ -6,7 +7,7 @@ import os
 import json
 import socket
 from CameraConfig import CameraConfig
-from ServoController import movePosition
+from MockServoController import movePosition
 
 configPath = "camera-config.json"
 
@@ -14,6 +15,36 @@ running = True
 cap = None
 ws = None
 config = None
+receiveStopped = True
+
+
+def receive_messages():
+    global ws, receiveStopped
+
+    while running and not receiveStopped:
+        try:
+            message = ws.recv()
+            if len(message) > 0:
+                # Process the received message here
+                messageType = struct.unpack('<i', message[:4])[0]
+                if messageType == 1:
+                    newPitch = struct.unpack('<f', message[4:8])[0]
+                    newYaw = struct.unpack('<f', message[8:12])[0]
+                    if config.hasMotor:
+                        movePosition(newPitch, newYaw)
+                    else:
+                        print("No camera, not moving servos")
+        except socket.timeout:
+            pass
+        except websocket._exceptions.WebSocketTimeoutException:
+            pass
+        except websocket._exceptions.WebSocketConnectionClosedException:
+            print("The connection was closed while reading a message")
+            receiveStopped = True
+        except:
+            print("An error occurred while reading a message")
+            receiveStopped = True
+
 
 if not os.path.exists(configPath):
     print("Error! No config file found.")
@@ -28,11 +59,18 @@ else:
         print("The config file should contain: cameraId, cameraToken, webSocketEndpoint.")
         running = False
 
+receive_thread = None
+
 while running:
     try:
         print("Connecting to the server at " + config.webSocketEndpoint)
         # Connect to the server using WebSocket (Important note! The port is 5000, not 5001! It doesn't work with 5001, I don't know why)
         ws = websocket.create_connection(config.webSocketEndpoint, timeout=8)
+
+        if receiveStopped:
+            receiveStopped = False
+            receive_thread = threading.Thread(target=receive_messages)
+            receive_thread.start()
 
         print("Did connect to the server, sending camera ID")
 
@@ -55,8 +93,6 @@ while running:
 
         print("Video capture started, will start sending video")
 
-        ws.settimeout(2)  # Set the time out to 2 seconds
-
         # Capture and encode the video
         while running:
             ret, frame = cap.read()
@@ -69,23 +105,6 @@ while running:
 
             # Send the video chunk to the server
             ws.send_binary(struct.pack('<I', len(data)) + data)
-
-            try:
-                message = ws.recv()
-                if (len(message) > 0):
-                    # Process the received message here
-                    newPitch = struct.unpack('<f', message[:4])[0]
-                    newYaw = struct.unpack('<f', message[4:8])[0]
-                    if (config.hasMotor):
-                        movePosition(newPitch, newYaw)
-                    else:
-                        print("No camera, not moving servos")
-            except socket.timeout:
-                print(
-                    "The connection timed out while reading a message, will try to continue sending video (socket.timeout))")
-            except websocket._exceptions.WebSocketTimeoutException:
-                print(
-                    "The connection timed out while reading a message, will try to continue sending video (websocket._exceptions.WebSocketTimeoutException))")
     except ConnectionResetError as error:
         print(
             "The established connection to the server was lost, will attempt to reconnect")
